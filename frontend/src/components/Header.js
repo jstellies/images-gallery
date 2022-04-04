93b90a19485b385fb52b1ef63dc9777252c00155
Rhodes, JE, Mr [23275103@sun.ac.za] <23275103@sun.ac.za>
2022-04-04 15:57:32 +0200

    first api call

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,9 +2,13 @@ import React from "react";
 import { Navbar } from "react-bootstrap";
 import { Container } from "react-bootstrap";
 
+const navbarStyle = {
+  backgroundColor: 'lightblue'
+};
+
 const Header = ({title}) => {
   return (
-    <Navbar bg="light" variant="light">
+    <Navbar style={navbarStyle} variant="light">
       <Container>
         <Navbar.Brand href="/">{title}</Navbar.Brand>
       </Container>
