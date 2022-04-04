a6ce759b0d9c02bf77854d73800be185800226a7
Rhodes, JE, Mr [23275103@sun.ac.za] <23275103@sun.ac.za>
2022-04-04 23:06:11 +0200

    Fix className errors in Welcome

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -2,11 +2,11 @@ import React from "react";
 
 const Welcome = () => {
   return (
-    <div class="jumbotron" className="bg-light p-4 rounded shadow-sm">
-        <h1 class="display-4">Images Gallery</h1>
-        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi pariatur cum rerum officiis. Eligendi, eum?</p>
-        <p class="lead">
-            <a class="btn btn-primary btn-lg" href="/" role="button">Learn more</a>
+    <div className="jumbotron bg-light p-4 rounded shadow-sm">
+        <h1 className="display-4">Images Gallery</h1>
+        <p className="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi pariatur cum rerum officiis. Eligendi, eum?</p>
+        <p className="lead">
+            <a className="btn btn-primary btn-lg" href="/" role="button">Learn more</a>
         </p>
     </div>
   );
